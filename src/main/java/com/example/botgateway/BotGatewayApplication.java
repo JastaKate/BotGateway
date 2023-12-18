df2c0c00b68c23d0aa7839a3645192d20786fc98
Kate <106038032+JastaKate@users.noreply.github.com>
2023-12-18 19:55:49 +0300

    done

---
+++ src/main/java/com/example/botgateway/BotGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.botgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/botgateway/BotGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
